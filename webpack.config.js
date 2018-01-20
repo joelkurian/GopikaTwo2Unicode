2b0d40e90fa459ceff7d62ff0550669556da18cc
Joel Kurian <joel@pedagogy.study>
2018-01-20 13:38:11 +0530

    webpack build changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
-const path = require('path');
+const path = require("path");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    module: {
-        rules: [{
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-        }]
-    }
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [new CopyWebpackPlugin([
+    { from: './src/index.html', to: 'index.html' },
+  ])],
+  devServer: {
+    contentBase: "./dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
 };
